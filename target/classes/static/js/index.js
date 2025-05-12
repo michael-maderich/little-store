03b46b35da2515cdaa8819df06ca38d7c668f76f
Michael Maderich <michael.maderich@gmail.com>
2025-05-12 00:42:09 -0400

    Stop tracking /target; remove it from the repo

--- target/classes/static/js/index.js
+++
@@ -1,6 +0,0 @@
-/*
-var el = document.querySelector('html');
-var style = window.getComputedStyle(el, null).getPropertyValue('font-size');
-var fontSize = parseFloat(style); 
-// now you have a proper float for the font size (yes, it can be a float, not just an integer)
-alert(fontSize);*/
